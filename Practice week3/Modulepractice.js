88dc591d7dd1fbcc16038ce54f7905ac9f367834
Faareha Raza <164369218+Faareha59@users.noreply.github.com>
2025-02-23 20:21:09 +0500

    Add files via upload

---
+++ Practice week3/Modulepractice.js
@@ -0,0 +1,9 @@
+function createArrayOfObjects() {
+    const arrayOfcars = [
+    { name: "Tesla", model: 145 },
+    { name: "Honda", model: 678 },
+    { name: "Suzuki",model: 735 }
+    ];
+    return arrayOfcars;
+}
+module.exports = createArrayOfObjects;
